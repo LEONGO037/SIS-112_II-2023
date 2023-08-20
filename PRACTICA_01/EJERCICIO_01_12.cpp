f657b66687a65927ae27298f6e38f0b6b14223da
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-19 21:50:33 -0400

    Create EJERCICIO_01_12.cpp
    Hice el ejercicio 12

---
+++ PRACTICA_01/EJERCICIO_01_12.cpp
@@ -0,0 +1,33 @@
+// Materia: Programacion I, Paralelo 4
+// Autor: Leonardo Delgado Medrano
+// Fecha creacion: 19/08/2023
+// Fecha modificacion: 19/08/2023
+// Numero de ejericio: 12
+// Problema planteado: Mostrar los primero 100 numero primos.
+#include<iostream>
+using namespace std;
+bool EsPrimo(int num)
+{
+    bool Primo = true;
+    for(int i=2;i<num/2;i++)
+    {
+        if (num%i==0)
+            Primo = false;
+    }
+    return Primo;
+}
+int main()
+{
+    cout<<"Los primeros 100 numeros primos son:"<<endl;
+    int num=2, j=0;
+    while(j<=100)
+    {
+        if (EsPrimo(num))
+        {
+            cout<<num<<endl;
+            j++;
+        }
+        num++;
+    }
+    return 0;
+}
