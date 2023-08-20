a23cbdb61dc60140f6ab98dd716cdfbf5ab5ca23
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-19 22:17:24 -0400

    Update EJERCICIO_01_13.cpp
    Corregí el numero de ejercicio

--- PRACTICA_01/EJERCICIO_01_13.cpp
+++ PRACTICA_01/EJERCICIO_01_13.cpp
@@ -2,7 +2,7 @@
 // Autor: Leonardo Delgado Medrano
 // Fecha creacion: 19/08/2023
 // Fecha modificacion: 19/08/2023
-// Numero de ejericio: 12
+// Numero de ejericio: 13
 // Problema planteado: Indique la cantidad de dígitos que tiene un numero entero.
 #include<iostream>
 using namespace std;
