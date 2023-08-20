624c41f6867cf68826df6a1ad4e1f5179fe1638e
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-19 20:00:18 -0400

    Update EJERCICIO_01_08.cpp
    corregi las tildes

--- PRACTICA_01/EJERCICIO_01_08.cpp
+++ PRACTICA_01/EJERCICIO_01_08.cpp
@@ -1,11 +1,11 @@
-// Materia: Programación I, Paralelo 4
+// Materia: Programacion I, Paralelo 4
 // Autor: Leonardo Delgado Medrano
-// Fecha creación: 17/08/2023
-// Fecha modificación: 17/08/2023
-// Número de ejericio: 8
+// Fecha creacion: 17/08/2023
+// Fecha modificacion: 17/08/2023
+// Numero de ejericio: 8
 // Problema planteado: En el estacionamiento del Megacenter se cobra 7 Bs la primera hora, si pasa de una
 //hora se cobra 5 Bs adicional por hora.
-//Ingrese la cantidad de horas que un vehículo está estacionado y calcule cuanto debe pagar.
+//Ingrese la cantidad de horas que un vehiculo esta estacionado y calcule cuanto debe pagar.
 #include<iostream>
 using namespace std;
 int main ()
