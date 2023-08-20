ec6cb7e43a52c6b4302659c2263944e8d405216e
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-19 22:01:13 -0400

    Create EJERCICIO_01_13.cpp
    Hice el ejercicio 13

---
+++ PRACTICA_01/EJERCICIO_01_13.cpp
@@ -0,0 +1,21 @@
+// Materia: Programacion I, Paralelo 4
+// Autor: Leonardo Delgado Medrano
+// Fecha creacion: 19/08/2023
+// Fecha modificacion: 19/08/2023
+// Numero de ejericio: 12
+// Problema planteado: Indique la cantidad de dígitos que tiene un numero entero.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int numero, digitos=0;
+    cout<<"Ingrese un numero"<<endl;
+    cin>>numero;
+    //Se ve cuantos digitos tiene el numero
+    while(numero>0)
+    {
+        numero/=10;
+        digitos++;
+    }
+    cout<<"El numero tiene "<<digitos<<" digitos"<<endl;
+}
