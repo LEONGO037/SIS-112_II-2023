490e58160ac6122704d5c7c30920303543695463
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-21 22:39:15 -0400

    Create EJERCICIO_01_53.cpp
    Hice el ejercicio 53

---
+++ PRACTICA_01/EJERCICIO_01_53.cpp
@@ -0,0 +1,23 @@
+// Materia: Programacion I, Paralelo 4
+// Autor: Leonardo Delgado Medrano
+// Fecha creacion: 21/08/2023
+// Fecha modificacion: 21/08/2023
+// Numero de ejericio: 53
+// Problema planteado: Escriba un programa que permita al usuario obtener los valores de las funciones trigonometricas
+#include<iostream>
+#include<cmath>
+#define Pi 3.14159
+using namespace std;
+int main()
+{
+    float angulo;
+    cout<<"Ingrese el angulo"<<endl;
+    cin>>angulo;
+    cout<<"El seno es "<<sin((angulo*Pi)/180.0)<<endl;
+    cout<<"El coseno es "<<cos((angulo*Pi)/180.0)<<endl;
+    cout<<"El tangente es "<<tan((angulo*Pi)/180.0)<<endl;
+    cout<<"El arcoseno es "<<asin((angulo*Pi)/180.0)<<endl;
+    cout<<"El arcocoseno es "<<acos((angulo*Pi)/180.0)<<endl;
+    cout<<"El arcotangente es "<<atan((angulo*Pi)/180.0)<<endl;
+    return 0;
+}
