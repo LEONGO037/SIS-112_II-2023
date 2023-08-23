baf86639ccc24228fbea29a6e1602228f86313de
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-22 22:11:13 -0400

    Update EJERCICIO_01_47.cpp
    agregue lo del bono por antigüedad

--- PRACTICA_01/EJERCICIO_01_47.cpp
+++ PRACTICA_01/EJERCICIO_01_47.cpp
@@ -1,7 +1,7 @@
 // Materia: Programacion I, Paralelo 4
 // Autor: Leonardo Delgado Medrano
 // Fecha creacion: 21/08/2023
-// Fecha modificacion: 21/08/2023
+// Fecha modificacion: 22/08/2023
 // Numero de ejericio: 47
 // Problema planteado: Un negocio de Cosmeticos de belleza, contratara a n vendedoras para la venta de sus productos.
 #include<iostream>
@@ -14,9 +14,26 @@ int main()
     cin>>empleado;
     for (int i=0;i<empleado;i++)
     {
+        int anios;
         float venta, ventatotal=0.0;
         cout<<"Ingrese el sueldo"<<endl;
         cin>>sueldo;
+        cout<<"Ingrese cuantos anios lleva trabajando"<<endl;
+        cin>>anios;
+        if(2<=anios && anios<=4)
+            sueldo+=(sueldo*0.05);
+        if(5<=anios && anios<=7)
+            sueldo+=(sueldo*0.11);
+        if(8<=anios && anios<=10)
+            sueldo+=(sueldo*0.18);
+        if(11<=anios && anios<=14)
+            sueldo+=(sueldo*0.26);
+        if(15<=anios && anios<=19)
+            sueldo+=(sueldo*0.34);
+        if(20<=anios && anios<=24)
+            sueldo+=(sueldo*0.42);
+        if(25<=anios)
+            sueldo+=(sueldo*0.5);
         //Se halla la suma de las 10 ventas
         for(int j=1;j<=10;j++)
         {
@@ -25,7 +42,7 @@ int main()
             ventatotal+=venta;
         }
         cout<<"Su comision por ventas es de "<<ventatotal*0.1<<endl;
-        cout<<"Su sueldo total contando la comision es de "<<sueldo+(ventatotal*0.1)<<endl;
+        cout<<"Su sueldo total contando la comision y su bono por antiguedad es de "<<sueldo+(ventatotal*0.1)<<endl;
     }
     return 0;
 }
